11f741a2fcfd4c3c8ff3d6c014c1ffb839096569
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-10-12 22:17:10 +0300

    ok 6

---
+++ my_select_6.py
@@ -0,0 +1,25 @@
+from src.models import Student, Group
+from src.db import session
+
+"""Знайти список студентів у певній групі.
+SELECT s.fullname  as 'Студент', g.name as 'Група'
+FROM [groups] g
+JOIN students s ON s.group_id  = g.id  
+WHERE g.id  = 3
+ORDER BY s.fullname  
+"""
+
+
+def select_last(group_id):
+    result = session.query(Student.fullname, Group.name) \
+        .select_from(Group)\
+        .join(Student)\
+        .where(Student.group_id == Group.id)\
+        .filter(Group.id == group_id)\
+        .all()
+
+    return result
+
+
+if __name__ == '__main__':
+    print(select_last(3))
